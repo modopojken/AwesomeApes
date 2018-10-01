7b2242766c1b1d77f33188e72f519eafcacf34c0
Jens Andreasson <jens.andreasson@ga.ntig.se>
2018-10-01 11:15:20 +0200

    Update login.php
    Lite kommentarer/förslag och TODO

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 // http://php.net/manual/en/language.variables.scope.php
-
+// TODO:
 // flytta till include för gitignore, vi har gått igenom detta
 // namnge variabler så att deras syfte är tydligt
 
@@ -9,7 +9,7 @@ session_start();
 $dbuser = "root";
 $dbpass = "";
 $dbh = new PDO('mysql:host=localhost;dbname=awesomeapes', $dbuser, $dbpass);
-
+// filtrera inte innan ni kontrollerar om formuläret skickats
 $filtered_username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 
 // kör dessa med filter och skicka sedan med dem till era metoder
@@ -23,7 +23,6 @@ if(isset($_POST["signup"])){
 	add_money($filtered_username, 10, $dbh);
 }
 
-
 function signup($username, $password, PDO $dbh){
 	$hashed_password =	password_hash($password, PASSWORD_DEFAULT);
 	$stmt = $dbh->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
@@ -31,6 +30,9 @@ function signup($username, $password, PDO $dbh){
 	$stmt->bindParam(':password', $hashed_password);
 	$stmt->execute();
 	echo "Created account for Mr." . $username;
+	// här kan ni använda header för att skicka vidare användaren
+	// http://php.net/manual/en/function.header.php
+	// vill ni ha med ett meddelande, testa GET eller js
 }
 
 function add_money($username, $amount, PDO $dbh)	{
@@ -38,7 +40,10 @@ function add_money($username, $amount, PDO $dbh)	{
 	$stmt->execute();
 }
 
-function login($input_username, $password, $dbh) {
+// metodhuvuden och konsekvent namngivning av variabler
+function login($input_username, $password, PDO $dbh) {
+	// bra med try catch, testa att skriva ut $e på exception
+	// går att använda för bättre felmeddelanden, duplicate username osv.
 	try {
 		$stmt = $dbh->prepare("SELECT * FROM users WHERE username = :username");
 		$stmt->bindParam(":username", $input_username);
@@ -46,6 +51,10 @@ function login($input_username, $password, $dbh) {
 		$row = $stmt->fetch();
 		if(password_verify($password, $row['password'])) {
 				echo "Logged in!";
+				// sätt session variabler
+				// forwarda user till sidan med header
+				// kontrollera session där och visa inloggad sida
+				// med utloggning
 		} else {
 			echo "Wrong password";
 		}
